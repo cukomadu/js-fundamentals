bf1808c8371d8e1b24bdce9f3da366457235a600
Chinyere Ukomadu <CUkomadu@Chinyeres-MacBook-Pro.local>
2016-11-15 17:13:32 -0600

    completed adventure mode and added algorithm notes to adventure mode for intro-objects.js

--- intro-objects.js
+++ intro-objects.js
@@ -67,28 +67,28 @@ console.assert(getFullNames(customers)[1] === "John Smith")
 // (which is, almost, what this is) should take a name input, and the dog should receive the 
 // assigned name.
 
-function generateDog(name){
+function GenerateDog(name){
     this.name = name
     this.legs = 4
     this.weight = 20
     this.color = "black"  
-    this.speak = function(string){
-        var newString = ''
-        var splitSentence = string.split(' ')
-        for(var i = 0; i < splitSentence.length; i++){
-            var replace = splitSentence[i].replace(splitSentence[i][0], 'r')
-            newString = newString + ' ' + replace
+    this.speak = function(str){
+        var newArray = []
+        var strToArray = str.split(' ')
+        for(var i = 0; i < strToArray.length; i++){
+            var exchange = strToArray[i].replace(strToArray[i][0], 'r')
+            newArray.push(exchange)
         }
-        return newString
-    }
+        return newArray.join(" ")
+    } 
 }
 
-var dog = new generateDog('rex')
+var dog = new GenerateDog('rex')
 
 console.assert(dog.legs === 4)
 console.assert(dog.name === 'rex')
 
-var dog = new generateDog('carl')
+var dog = new GenerateDog('carl')
 console.assert(dog.name === 'carl')
 
 
@@ -142,16 +142,37 @@ var text = "It’s obviously not the case, but T’Challa—the Black Panther an
 Comic book creators, like all story-tellers, get great mileage out of myth and history. But given the society we live in, some people’s myths are privileged over others. Some of that is changing, no doubt. In the more recent incarnations of T’Challa you can see Christopher Priest invoking the language of the Hausa or Reginald Hudlin employing the legacy of colonialism. These were shrewd artistic decisions, rooted in the fact that anyone writing Black Panther enjoys an immediate, if paradoxical, advantage: the black diaspora is terra incognita for much of the world. What does the broader world really know of Adowa? Of Nanny and Cudjoe? Of the Maji-Maji rebellion? Of Legba and Oshun?  Of Shine? Of High John The Conqueror? T’Challa’s writers have always enjoyed access to a rich and under-utilized pool of allusion and invocation."
 
 
-function cleanText(text) {
-    for(var i = 0; i < text.length; i++){
-        var cleanNow = text.replace(/[-:'?,.""_]/g,"")
+function removePunc(strOfWords){
+     var puncDictionary = [",", ".", "’", "—", "?", ":", "-", "”", "“"]
+     var strToArray = strOfWords.split("")
+    for (var i = 0; i < strToArray.length; i++) {
+          for (var j = 0; j < puncDictionary.length; j++) {
+               if(strToArray[i] === puncDictionary[j]){
+                    strToArray[i] = ""
+               }
+          }
     }
-  return cleanNow
+    return strToArray.join("").toLowerCase()
 }
 
-var getCounts = function(text){
-    var removePunctiation = cleanText(text)
 
+var getCounts = function(text){
+    var frequencyObj = {},
+        transformedText = removePunc(text),
+        textToArray = transformedText.split(" ")
+
+    for(var i = 0; i < textToArray.length; i++){
+        var word = textToArray[i]
+
+        if(frequencyObj[word]){
+            frequencyObj[word] += 1
+            
+        } else {
+            frequencyObj[word] = 1
+        }
+    }
+        
+    return frequencyObj
 }
 
 var wordFrequencyObject = getCounts(text)
@@ -173,11 +194,31 @@ console.assert(wordFrequencyObject.black === 5)
 // Write a function called reverseObject(). It should take as input an object, 
 // and it should output a new object where the keys and values are reversed.
 
+/* 
+Algorithm
+1. Define a function called reverseObject() with input as object
+2. Define an empty output object
+3. Define a Loop through the key in the object 
+4. Assign the current value at key to a new variable called newKey as you loop 
+5. Reverse the assignment key = value and value = key    
+*/
+
 
 var object = {
     occupants: 4,
     apartment_no: "2b",
     structural_integrity: "failing"
+}
+
+function reverseObject(obj){
+    var newObj = {}
+    for(var key in obj){
+        //detailed solution
+        //var newKey = obj[key]
+        //newObj[newKey] = key 
+        newObj[obj[key]] = key
+    }
+    return newObj
 }
 
 var reversed = reverseObject(object)
@@ -190,10 +231,28 @@ console.assert(reversed['2b'] === 'apartment_no')
 // objects, and it should output an array of objects with the keys and values
 // reversed.
 
+/* 
+Algorithm
+1. Define a function called reverseAll() with input as array of objects
+2. Define an empty output array
+3. Define a Loop through the array of objects
+4. Invoke the reverseObject function and pass each array element as a parameter
+5. Push reversed element into output array and return output array
+*/
+
 var users = [{obama: 'president@gmail.com',hobby: 'basketball'},{trump: 'americamoneywin@yahoo.com', hobby:'dealmaking'},{bush: 'jeb!@hotmail.com',hobby:'portraiture'}]
 // should yield: [{'president@gmail.com': 'obama',basketball: 'hobby'}, ....]
 
-var flippedUsers = reverseObjects(users)
+function reverseAll(arrayOfObjects){
+    var outputArray = []
+    for(var i = 0; i < arrayOfObjects.length; i++){
+        var reverse = reverseObject(arrayOfObjects[i])
+        outputArray.push(reverse)
+    }
+    return outputArray
+}
+
+var flippedUsers = reverseAll(users)
 
 console.assert(flippedUsers[0]['president@gmail.com'] === 'obama')
 console.assert(flippedUsers[1]['americamoneywin@yahoo.com'] === 'trump')
@@ -215,6 +274,38 @@ var plays = [
     {title: "Death of a Salesman", author: "Arthur Miller", year: 1949},
     {title: "Two Blind Mice", author: "Samuel and Bella Spewack", year: 1949}
 ]
+
+/* 
+Algorithm
+1. Define a function called where() with input as array of objects and object property
+2. Define an empty output array
+3. Define a Loop through the array of objects
+4. Define a Loop through the object property parameter
+4. Define a condition that checks the key value at the current array position in the loop with the key value of the object property parameter
+5. If there is a match, Push the object at the current array position to the output array. If no match, keep looping
+6. Return the output array.
+*/
+
+
+function where(arrayOfObjects, objectProp){
+    var outputArray = []
+    for (var i = 0; i < arrayOfObjects.length; i++) {
+        var currentTarget = arrayOfObjects[i],
+            truthy = true
+
+        for (var key in objectProp){ 
+            if(currentTarget[key] !== objectProp[key]){
+                truthy = false
+            }
+        }
+
+        if(truthy){
+            outputArray.push(currentTarget)
+        }    
+    }
+    return outputArray
+} 
+
 
 var sh8spr = where(plays, {author: "Shakespeare"})
 console.assert(sh8spr instanceof Array)
@@ -238,9 +329,22 @@ console.assert(midcentury.length === 2)
 // return value. Use the example in the console.assert to understand
 // exactly how you should write the method. Including the period! 
 
-var politeObject = {
-    name: "Frank"
+/* Algorithm
+1. Define an object constructor called Polite with a name attribute and a personalize method
+2. Define a variable called politeObject and assign a new instance of the Polite constructor to it
+3. Call the personalize method on the politeObject 
+*/
+
+function Polite(name){
+    this.name = name
+    this.personalize = function(func){
+        var intro = "Hi, my name is " + this.name + ", " + "and the result is " + func() + "."
+        return intro
+    }
 }
+
+
+var politeObject = new Polite("Frank")
 
 var helloWorld = function() {
     return "hello world"
